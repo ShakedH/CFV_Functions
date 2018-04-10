530f54cf25ba98bb8c11bc3ae3a9c84790521ef8
Shaked Hazon <shkdhazon@gmail.com>
2018-04-10 17:52:43 +0300

    Updated According to Azure Function

--- AudioExtractor-python36.py
+++ AudioExtractor-python36.py
@@ -2,12 +2,14 @@ import subprocess
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'myenv/Lib/site-packages')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'myenv/Lib/site-packages')))
 import numpy as np
 from azure.storage.blob import BlockBlobService
 from azure.storage.queue import QueueService
 import json
 import base64
+import io
+import scipy.io.wavfile as wavfile
 
 account_name = 'cfvtes9c07'
 account_key = 'DSTJn6a1dS9aaoJuuw6ZOsnrsiW9V1jODJyHtekkYkc3BWofGVQjS6/ICWO7v51VUpTHSoiZXVvDI66uqTnOJQ=='
@@ -15,38 +17,46 @@ audio_container_name = "audiocontainer"
 block_blob_service = BlockBlobService(account_name, account_key)
 queue_service = QueueService(account_name=account_name, account_key=account_key)
 videos_container_URL = "https://cfvtes9c07.blob.core.windows.net/videoscontainer"
-queue_name = "extractor-to-splitter-q"
-
+queue_name = "extractor-to-asr-q"
 
 def extract_audio_from_video(video_id):
     input_path = videos_container_URL + "/" + video_id
-    print ('input_path: ' + input_path)
-    command = ['ffmpeg',
-               '-i', input_path,
-               '-vn',
-               '-f', 'mp3',
-               'pipe:1'
-               ]
-    pipe = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print ('input_path: '+ input_path)
+    command = [ 'ffmpeg',
+                '-i', input_path,
+                '-vn',
+                '-f','wav',
+                'pipe:1'
+                ]
+    pipe = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
     stdoutdata, stderr = pipe.communicate()
-    audio_array = np.fromstring(stdoutdata, dtype="int16")
-    audio_blob_name = video_id.replace('.mp4', '.mp3')
+    stdoutdata = bytes(stdoutdata)
+    riff_chunk_size = len(stdoutdata) - 8
+    q = riff_chunk_size
+    print ('riff size' + str(q))
+    b = []
+    for i in range(4):
+        q, r = divmod(q, 256)
+        b.append(r)
+    riff = stdoutdata[:4] + bytes(b) + stdoutdata[8:]
+    rate, audio_array = wavfile.read(io.BytesIO(riff))
+    duration_seconds = len(audio_array) / rate
+    #audio_array = np.fromstring(stdoutdata, dtype="int16")
+    audio_blob_name = video_id.replace('.mp4', '.wav')
     print (' received ffmpeg output byte array')
-    block_blob_service.create_blob_from_bytes(audio_container_name, audio_blob_name, audio_array.tobytes())
+    block_blob_service.create_blob_from_bytes(audio_container_name,audio_blob_name,riff)
     print ('uploaded audio file to blob')
-    return audio_blob_name
+    return audio_blob_name, duration_seconds
 
-
-def put_message_in_queue(queue_name, video_id, audio_blob_name):
-    print ('Creating message for queue:' + queue_name)
-    message = {"video_id": video_id, "audio_blob_name": audio_blob_name}
+def put_message_in_queue(queue_name, video_id, audio_blob_name, duration_seconds):
+    print ('Creating message for queue:'+ queue_name )
+    message = {"ID": video_id, "file_name": audio_blob_name, "duration": duration_seconds}
     message = json.dumps(message)
     message = base64.b64encode(message.encode("ascii")).decode()
     queue_service.put_message(queue_name, message)
     print ("Sent message:" + message)
 
-
 if __name__ == "__main__":
     video_id = open(os.environ["inputMessage"]).read()
-    audio_blob_name = extract_audio_from_video(video_id)
-    put_message_in_queue(queue_name, video_id, audio_blob_name)
+    audio_blob_name, duration_seconds = extract_audio_from_video(video_id)
+    put_message_in_queue(queue_name, video_id, audio_blob_name, duration_seconds)
